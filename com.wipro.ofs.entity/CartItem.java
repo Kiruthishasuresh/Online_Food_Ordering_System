7b3dd106902b047372d18ade9c1c1a391127fe05
Kiruthisha <sureshkiruthisha@gmail.com>
2026-01-22 22:08:19 +0530

    Create CartItem.java

---
+++ com.wipro.ofs.entity/CartItem.java
@@ -0,0 +1,22 @@
+package com.wipro.ofs.entity;
+public class CartItem {
+	private String itemId;
+	private int quantity;
+	public CartItem(String itemId, int quantity) {
+		super();
+		this.itemId = itemId;
+		this.quantity = quantity;
+	}
+	public String getItemId() {
+		return itemId;
+	}
+	public void setItemId(String itemId) {
+		this.itemId = itemId;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+}
